21139e5726f33d3fb59149d957c72d3b43436093
Leilamica <163433407+Leilamica@users.noreply.github.com>
2024-10-09 11:28:36 +0800

    Add files via upload

---
+++ Name.Data/InfoData.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Name.model;
+using SMTP;
+
+namespace Name.Data
+{
+    public class InfoData
+    {
+        string _connectionString = "Data Source=DESKTOP-8GMKO0I\\SQLEXPRESS01;Initial Catalog=NameDb;Integrated Security=True;";
+
+        public List<Info> GetInfos()
+        {
+            List<Info> infos = new List<Info>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT Name, Description FROM Infos";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Info info = new Info
+                    {
+                        Name = reader["Name"].ToString(),
+                        Description = reader["Description"].ToString()
+                    };
+
+                    infos.Add(info);
+                }
+                connection.Close();
+            }
+
+            return infos;
+        }
+        private readonly smtp _emailService;
+
+        public InfoData()
+        {
+            _emailService = new smtp();
+        }
+
+        public int AddInfo(Info info)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO Infos (Name, Description) VALUES (@Name, @Description)";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", info.Name);
+                command.Parameters.AddWithValue("@Description", info.Description);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                if (rowsAffected > 0)
+                {
+                    _emailService.SendEmail("leilamicacenita@gmail.com", "New Info Added", $"Info added: {info.Name}");
+                }
+
+                return rowsAffected;
+            }
+        }
+
+        public int UpdateInfo(Info info)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "UPDATE Infos SET Description = @Description WHERE Name = @Name";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", info.Name);
+                command.Parameters.AddWithValue("@Description", info.Description);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                if (rowsAffected > 0)
+                {
+                    _emailService.SendEmail("leilamicacenita@gmail.com", "New Info Updated", $"Info updated: {info.Name}");
+                }
+
+                return rowsAffected;
+            }
+        }
+    
+
+public int DeleteInfo(string infoName)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "DELETE FROM Infos WHERE Name = @Name";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", infoName);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                return rowsAffected;
+            }
+        }
+
+    }
+}
---
+++ Name.business/InfoServices.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Name.model;
+using Name.Data;
+
+namespace Name.business;
+
+public class InfoServices
+{
+    private InfoData _infoData;
+
+    public InfoServices()
+    {
+        _infoData = new InfoData();
+    }
+
+    public bool AddInfo(Info info)
+    {
+        try
+        {
+            int rowsAffected = _infoData.AddInfo(info);
+            return rowsAffected > 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error adding info: {ex.Message}");
+            return false;
+        }
+    }
+
+    public bool UpdateInfo(Info info)
+    {
+        try
+        {
+            int rowsAffected = _infoData.UpdateInfo(info);
+            return rowsAffected > 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error updating info: {ex.Message}");
+            return false;
+        }
+    }
+
+    public bool DeleteInfo(string infoName)
+    {
+        try
+        {
+            int rowsAffected = _infoData.DeleteInfo(infoName);
+            return rowsAffected > 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error deleting info: {ex.Message}");
+            return false;
+        }
+    }
+
+    public List<Info> GetAllInfos()
+    {
+        try
+        {
+            return _infoData.GetInfos();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error retrieving infos: {ex.Message}");
+            return new List<Info>();
+        }
+    }
+}
